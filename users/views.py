fe6b74f7462e67ae691a604931ec422e0f9ba3e0
f59lbb0 <luis.bernues@ficosa.com>
2020-05-20 13:03:33 +0200

    Signed up option added

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -26,3 +27,17 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, "users/login.html", {"message": "Logged out."})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
